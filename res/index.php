5e481650dae49c79c61d15be205d2412efafedb0
TangMonk <wytwyt02@gmail.com>
2020-06-05 11:18:38 +0800

    add index mulu

---
+++ res/index.php
@@ -0,0 +1,301 @@
+<html><!-- InstanceBegin template="/Templates/wenjian.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<script> 
+   //js上一页 下一页 代码
+   //说明：文件名必须是abc-123.htm的格式，分为四个部分：其中abc是任意名字；-是连字符不能少；123只能是数字；后缀必须是.htm
+var url=window.location.href; 
+var n=url.split("/")[url.split("/").length-1].split(".")[0];  
+var num= n.replace(/[^0-9]/ig,"");
+var pre=url.lastIndexOf("-")+1;
+var pre2=url.substring(0,pre);
+
+   //上一页
+   function up(){
+     var pageno=num-1;
+     if(pageno==0){
+         alert("当前已经是第一页!");
+         return false;
+     }
+     window.location.href=pre2+pageno+".htm";
+   }
+   //下一页
+   function down(){ 
+     var pageno=num-(-1);
+     if(pageno==1000){
+         alert("当前已经是最后一页!");
+         return false;
+     }
+     window.location.href=pre2+pageno+".htm";
+   }
+</script>
+
+<script> 
+   //js上一页 下一页 代码
+   //说明：文件名必须是abc-123.htm的格式，分为四个部分：其中abc是任意名字；-是连字符不能少；123只能是数字；后缀必须是.htm
+var url=window.location.href; 
+var n=url.split("/")[url.split("/").length-1].split(".")[0];  
+var num= n.replace(/[^0-9]/ig,"");
+var pre=url.lastIndexOf("-")+1;
+var pre2=url.substring(0,pre);
+
+   //上一页
+   function up(){
+     var pageno=num-1;
+     if(pageno==0){
+         alert("当前已经是第一页!");
+         return false;
+     }
+     window.location.href=pre2+pageno+".htm";
+   }
+   //下一页
+   function down(){ 
+     var pageno=num-(-1);
+     if(pageno==1000){
+         alert("当前已经是最后一页!");
+         return false;
+     }
+     window.location.href=pre2+pageno+".htm";
+   }
+</script>
+<!-- InstanceBeginEditable name="doctitle" -->
+<title></title>
+<!-- InstanceEndEditable -->
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<meta name="keywords" content="佛教与佛学,菩提道次第,佛教文艺,佛教心理学,禅定与禅修,buda
+">
+
+<link href="/puti.css" rel="stylesheet" type="text/css">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+//-->
+</script>
+<!-- putixin.com Baidu tongji analytics -->
+<script>
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?13dd5c046c14d2de56c550ab86bb904e";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(hm, s);
+})();
+</script>
+</head>
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- InstanceBeginEditable name="EditRegion4" --> <!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable char="EditRegion5" --> <!-- InstanceEndEditable -->
+<div id="Layer2" style="position:absolute; left:13px; top:14px; width:65px; height:65px; z-index:9; visibility: visible;" class="font4"><img src="../../images/index-logo.gif" width="65" height="65" border="0"></div>
+<table width="778" height="52" border="0" cellpadding="0" cellspacing="0" bgcolor="#FEF1DA">
+<!--DWLayoutTable-->
+<tbody>
+<tr>
+<td width="75" height="52"></td>
+<td width="703"><img src="../../fls/images/daoshi-t1.jpg" width="704" height="57"></td>
+</tr>
+</tbody>
+</table>
+<table width="778" border="0" cellpadding="0" cellspacing="0"  bgcolor="#FEF1DA">
+<!--DWLayoutTable-->
+<tbody>
+<tr>
+<td width="55" height="20" class="font2"></td>
+<td width="18" bgcolor="#556aa7" class="font2"></td>
+<td width="169" height="20"  bgcolor="#556aa7" class="font4"> <div align="center"><font color="#FFFFFF">www.putixin.com</font></div></td>
+<td width="27" valign="bottom" bgcolor="#00468e"> <div align="center"></div></td>
+<td width="159" valign="bottom" bgcolor="#00468e"> <div align="center"><a href="../../index.htm" class="BAR">返回《一日一读》</a></div></td>
+<td width="153" align="left" valign="bottom" bgcolor="#00468e"> <div align="center"><a href="../con-index.htm" class="BAR">返回《连载专栏》</a></div></td>
+<td width="197" align="left" valign="bottom" bgcolor="#00468e"><!--DWLayoutEmptyCell-->&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<table width="778" border="0" cellpadding="0" cellspacing="0">
+<!--DWLayoutTable-->
+<!-- InstanceBeginEditable name="EditRegion7" -->
+<tr>
+<td width="30" valign="top" bgcolor="#FFE7B9" ><div id="Layer6" style="position:absolute; width:187px; height:44px; z-index:18; left: 38px; top: 87px;">
+<p align="center" class="p13"><font color="#008800" face="宋体"> <span class="p13-6"><a href="http://www.putixin.com/art" target="_blank"><font color="#FF0000">佛教文艺演出</font></a></span><br>
+<a href="http://www.putixin.com/art" target="_blank">（视频在线播放）</a></font></p>
+</div></td>
+<td width="202" valign="top" bgcolor="#FFDFA4" ><table width="212" height="212" border="0">
+<tr>
+
+          <td width="212" height="40"><p>&nbsp;</p>
+          <p>&nbsp;</p></td>
+</tr>
+</table>
+<table width="201" height="56" border="3" align="center" cellpadding="0" cellspacing="3" bordercolor="#FCD38F" bgcolor="#FFECC8">
+<tr>
+<td height="25" colspan="2" align="center" valign="middle"> <div align="center" class="p13"><a href="../con-index.htm"><font color="#008080"><strong>连载专栏</strong></font></a></div></td>
+</tr>
+<tr>
+<td width="93" height="25" align="center" valign="middle"> <div align="center"><a href="../../index.htm" class="BAR2" onFocus="if(this.blur)this.blur()">首页(一日一读)</a></div></td>
+<td width="89" align="center" valign="middle"><a href="../con-index.htm#fxjj" class="BAR2" onFocus="if(this.blur)this.blur()">佛学讲记</a></td>
+</tr>
+<tr>
+<td height="25" align="center" valign="middle"><a href="../con-index.htm#zcll" class="BAR2" onFocus="if(this.blur)this.blur()">政策理论</a></td>
+<td align="center" valign="middle"><a href="../con-index.htm#wx" class="BAR2" onFocus="if(this.blur)this.blur()">文
+学</a></td>
+</tr>
+<tr>
+<td height="25" align="center" valign="middle"><a href="../con-index.htm#dtbd" class="BAR2" onFocus="if(this.blur)this.blur()">动态报道</a></td>
+<td align="center" valign="middle"><a href="http://www.putixin.com/psy" class="BAR2" onFocus="if(this.blur)this.blur()">佛教心理学</a></td>
+</tr>
+<tr>
+<td height="25" align="center" valign="middle"><a href="../con-index.htm#jsfj" class="BAR2" onFocus="if(this.blur)this.blur()">介绍佛教</a></td>
+<td align="center" valign="middle"><a href="http://www.putixin.com/chan/chan-index.htm" class="BAR2" onFocus="if(this.blur)this.blur()">佛学禅定</a></td>
+</tr>
+<tr>
+<td height="25" align="center" valign="middle"><a href="../con-index.htm#jsft" class="BAR2" onFocus="if(this.blur)this.blur()">介绍佛陀</a></td>
+<td align="center" valign="middle"><a href="../con-index.htm#fjgs" class="BAR2" onFocus="if(this.blur)this.blur()">佛教故事</a></td>
+</tr>
+<tr>
+<td height="25" align="center" valign="middle"><a href="../con-index.htm#rwjs" class="BAR2" onFocus="if(this.blur)this.blur()">人物介绍</a></td>
+<td align="center" valign="middle"><a href="../con-index.htm#shdjy" class="BAR2" onFocus="if(this.blur)this.blur()">生活的教育</a></td>
+</tr>
+<tr>
+<td height="25" align="center" valign="middle"><a href="../con-index.htm#ymgy" class="BAR2" onFocus="if(this.blur)this.blur()">幽默格言</a></td>
+<td align="center" valign="middle"><a href="../con-index.htm#hnwfj" class="BAR2" onFocus="if(this.blur)this.blur()">海内外佛教</a></td>
+</tr>
+</table>
+
+</td>
+<td width="1" bgcolor="#FFECC8"></td>
+<td width="535"  valign="top" bgcolor="#FFECC8"><table width="100%" border="0" align="center" cellpadding="5" cellspacing="0">
+<tr align="center" valign="middle" class="font10Copy2">
+<td width="44%" height="33" align="left"><span class="font2">　　</span></td>
+<td width="10%">&nbsp;</td>
+          <td width="46%" align="left">　　　　　　　　　　　<span class="font2"><a href="fptxxxgf-02.htm"></a></span></td>
+</tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td>&nbsp;</td>
+</tr>
+</table>
+<table width="95%" border="0" align="center" cellpadding="5" cellspacing="0">
+<tr valign="top">
+<td class="font6"></td>
+</tr>
+<tr>
+<td height="103" valign="top">
+<div align="center">
+<?php
+$doc = new DOMDocument;
+$doc->preserveWhiteSpace = false;
+
+$filename = basename(__FILE__);
+$pre_filename = preg_split('/-/', $filename)[0];
+
+
+//$doc->load($filename);
+//$xpath = new DOMXPath($doc);
+//$query = '/html/body/table[3]/tbody/tr/td[4]/table[3]/tbody/tr[2]/td/div/p[1]/span/strong/font/font';
+
+
+?>
+<p>&nbsp;</p              <p class="font8"><font color="#750000">text_to_replace 目录</font></p>
+<p style="margin-top:-20" class="p13">──────────────────────────────</p>
+              <p style="margin-top:-20" class="font10">&nbsp;>
+</p>
+</div></td>
+</tr>
+<tr class="p13">
+<td height="1218" valign="top">
+<div align="center"> 
+        <table width="95%" border="1" cellspacing="1">
+			
+			
+			<?php
+	
+			$files = array_diff(scandir('.'), array('.', '..'));
+			sort($files, SORT_NATURAL);
+			$tr_times = 0;
+			
+			foreach($files as $file){
+				$number = abs((int) filter_var($file, FILTER_SANITIZE_NUMBER_INT));
+				if($number == 0)
+					continue;
+				
+				if($tr_times == 8){
+					echo '<td width="11%"><span class="font1"><a href="' . $file . '" >' . $number . '</a></span></td>';
+					echo '</tr>';
+					
+					echo '<tr align="center" bordercolor="#FCD38F" bgcolor="#FFECC8"> ';
+					
+					$tr_times = 0;
+				}else if ($tr_times == 0){
+					
+					echo '<tr align="center" bordercolor="#FCD38F" bgcolor="#FFECC8"> ';
+					echo '<td width="11%"><span class="font1"><a href="' . $file . '" >' . $number . '</a></span></td>';
+					$tr_times ++;
+				}else{
+					echo '<td width="11%"><span class="font1"><a href="' . $file . '" >' . $number . '</a></span></td>';
+					$tr_times ++;	
+				}
+				
+				
+			}
+			
+			
+			?>
+			  
+                
+              </table>
+              <p align="left" class="h21copy1">&nbsp;</p>
+</div></td>
+</tr>
+</table>
+<table width="100%" border="0" align="center" cellpadding="5" cellspacing="0">
+<tr align="center" valign="middle" class="font10Copy2">
+<td width="44%" height="33" align="left">&nbsp;</td>
+<td width="10%">&nbsp;</td>
+<td width="46%" align="left">&nbsp;</td>
+</tr>
+</table></td>
+</tr>
+<!-- InstanceEndEditable -->
+</table>
+<table width="778" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td width="20"  height="1" bgcolor="#ffecc8"  class="font1"></td>
+<td width="730" height="1" bgcolor="#666666" class="font1"></td>
+<td width="28"  height="1" bgcolor="#ffecc8"  class="font1"></td>
+</tr>
+</table>
+
+<table width="778" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFECC8">
+<tr>
+<td width="200" height="66">&nbsp;</td>
+<td width="61" rowspan="2" align="center" valign="middle"><a href="http://www.putixin.com" ><img src="/images/index-logo.gif" alt="佛教菩提心—佛教文化、佛学讲记、佛教心理、佛学禅定、佛教故事……" width="45" height="45" border="0"></a></td>
+<td width="420" height="66" valign="bottom" class="font3">
+<div align="center"><font color="#666666"><span class="font3"><font color="#555555"><img src="/images/email.jpg">
+<font color="#555555"> 　
+</font></font></span><font color="#555555"><br>
+<font color="#777777"><br>
+</font><font color="#555555"><span class="font3copy">菩提心网站版权所有，转载请注明出处，翻印须征得本站授权</span></font><font color="#777777">
+</font></font></font></div></td>
+<td width="97" height="66" valign="bottom"> </td>
+</tr>
+<tr>
+<td height="10">&nbsp;</td>
+<td height="10">&nbsp;</td>
+<td height="10">&nbsp;</td>
+</tr>
+</table>
+<!-- InstanceBeginRepeat name="RepeatRegion1" --><!-- InstanceBeginRepeatEntry --> 
+<!-- InstanceBeginEditable name="EditRegion6" --> 
+<div id="Layer5" style="position:absolute; width:100px; height:26px; z-index:26; left: 658px; top: 26px;"><span style="font-size: 9pt"> 
+  </span> <font color="#BF2600"> <a href="javascript%20StranBody();" id="StranLink" name="StranLink">繁體中文</a> 
+  <br>
+  <script language="JavaScript1.1~JavaScript1.1" src="include/Language.js" type="text/javascript"></script>
+  </font></div>
+<!-- InstanceEndEditable --><!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> 
+</body>
+
+
+<!-- InstanceEnd --></html>
--- unit1.pas
+++ unit1.pas
@@ -26,6 +26,7 @@ type
 
 var
   Form1: TForm1;
+  FilePrefixName: string;
 
 implementation
 
@@ -34,40 +35,42 @@ implementation
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  titleResourceStream: TResourceStream;
+  titleFile: TStringList;
 begin
-
+  titleResourceStream :=
+    TResourceStream.Create(HInstance, 'TITLES', RT_RCDATA);
+  titleFile := TStringList.Create;
+  titleFile.LoadFromStream(titleResourceStream);
+  titleFile.SaveToFile(GetCurrentDir + '\' + 'titles.txt');
 end;
 
 procedure TForm1.FormDropFiles(Sender: TObject; const FileNames: array of string);
 var
   AFile, Ext, Content, FileName, JsScript, FileEncoding, PrePage,
-  NextPage, Mulu, FileDir: string;
-  aStringList, JsScriptList: TStringList;
+  NextPage, Mulu, FileDir, TitleName, title, TitleValueName,
+  indexphpString: string;
+  aStringList, JsScriptList, zerozerozeroHTMLFile, indexphpFile,
+  titleFile, TitleKeyValuePair: TStringList;
   ReplacedContent: RegExprString;
-  RegexObj, PrePageLink, NextPageLink, MuluReg, FileNumberReg: TRegExpr;
+  RegexObj, PrePageLink, NextPageLink, MuluReg, FileNumberReg,
+  FilePrefixNameReg: TRegExpr;
   FileNumber: longint;
-  JsScriptResourceStream: TResourceStream;
+  JsScriptResourceStream, indexphpResourceStream, zerozerozeroResourceStream:
+  TResourceStream;
+  TitleNameArray, titleArray: TStringArray;
 begin
 
   StringGrid1.Clear;
   StringGrid1.RowCount := 1;
   ProgressBar1.Max := Length(FileNames);
+
+  titleFile := TStringList.Create;
+  titleFile.LoadFromFile(GetCurrentDir + '\titles.txt');
+
   for AFile in FileNames do
   begin
-    aStringList := TStringList.Create;
-    JsScriptResourceStream := TResourceStream.Create(HInstance,
-      'FLOAT_PAGE_HTML', RT_RCDATA);
-    JsScriptList := TStringList.Create;
-    JsScriptList.LoadFromStream(JsScriptResourceStream);
-    JsScript := JsScriptList.Text;
-
-    RegexObj := TRegExpr.Create;
-    PrePageLink := TRegExpr.Create('<a.+href="(.+).+上一页');
-    NextPageLink := TRegExpr.Create('<a.+href="(.+).+下一页');
-    MuluReg := TRegExpr.Create('.+-(\d+)');
-    RegexObj.Expression := '<div id="pagination"';
-    FileNumberReg := TRegExpr.Create('\d+');
-    RegExpr.RegExprModifierS := True;
 
     Ext := ExtractFileExt(AFile);
 
@@ -75,9 +78,41 @@ begin
     begin
       ProgressBar1.Position := ProgressBar1.Position + 1;
 
+      aStringList := TStringList.Create;
+
+      JsScriptResourceStream :=
+        TResourceStream.Create(HInstance, 'FLOAT_PAGE_HTML', RT_RCDATA);
+      JsScriptList := TStringList.Create;
+      JsScriptList.LoadFromStream(JsScriptResourceStream);
+      JsScript := JsScriptList.Text;
+
+      indexphpResourceStream :=
+        TResourceStream.Create(HInstance, 'INDEX.PHP', RT_RCDATA);
+      indexphpFile := TStringList.Create;
+      indexphpFile.LoadFromStream(indexphpResourceStream);
+
+      RegexObj := TRegExpr.Create;
+      PrePageLink := TRegExpr.Create('<a.+href="(.+).+上一页');
+      NextPageLink := TRegExpr.Create('<a.+href="(.+).+下一页');
+      MuluReg := TRegExpr.Create('.+-(\d+)');
+      RegexObj.Expression := '<div id="pagination"';
+      FileNumberReg := TRegExpr.Create('\d+');
+
+      RegExpr.RegExprModifierS := True;
+
       FileName := ExtractFileName(AFile);
       FileDir := ExtractFileDir(AFile);
       FileNumberReg.Exec(FileName);
+
+      TitleNameArray := FileDir.Split('\');
+      TitleName := TitleNameArray[high(TitleNameArray)];
+      TitleValueName := titleFile.Values[TitleName];
+
+      if TitleValueName = '' then begin
+        ShowMessage(TitleName + ' 还没有定义标题，请在titles.txt中定义文章标题');
+        continue;
+      end;
+
       FileNumber := StrToInt(FileNumberReg.Match[0]);
       if FileNumberReg.Match[0] = '000' then
         continue;
@@ -86,12 +121,11 @@ begin
       FileEncoding := GuessEncoding(aStringList.Text);
       Content := ConvertEncoding(aStringList.Text, FileEncoding, EncodingUTF8);
 
-      //if not RegexObj.Exec(Content) then
-      //begin
-      //Mulu := ReplaceRegExpr('\d+', FileName, '000', False);
-      Mulu:= 'index.php';
+
+      Mulu := 'index.php';
       PrePage := ReplaceRegExpr('\d+', FileName, Format('%.2d',
         [FileNumber - 1]), False);
+
       NextPage := ReplaceRegExpr('\d+', FileName,
         Format('%.2d', [FileNumber + 1]), False);
 
@@ -144,20 +178,30 @@ begin
       aStringList.Text := ConvertEncoding(ReplacedContent, EncodingUTF8, FileEncoding);
       aStringList.SaveToFile(AFile);
       StringGrid1.InsertRowWithValues(1, [FileName, '已替换']);
+
+      aStringList.Free;
+      RegexObj.Free;
+      PrePageLink.Free;
+      NextPageLink.Free;
+      MuluReg.Free;
+      FileNumberReg.Free;
+
+      //保存index.php文件
+      indexphpString := ConvertEncoding(indexphpFile.text, GuessEncoding(indexphpFile.text), EncodingUTF8);
+      indexphpString := indexphpString.Replace('text_to_replace', TitleValueName);
+      indexphpFile.Text := ConvertEncoding(indexphpString, EncodingUTF8, GuessEncoding(indexphpFile.text));;
+      indexphpFile.SaveToFile(FileDir + '\' + 'index.php');
+
+      ProgressBar1.Position := 0;
     end
     else
     begin
-      //StringGrid1.InsertRowWithValues(1, [FileName, '文件格式错误']);
+      ShowMessage(FileName + '文件格式错误');
     end;
-    aStringList.Free;
 
-    RegexObj.Free;
-    PrePageLink.Free;
-    NextPageLink.Free;
-    MuluReg.Free;
-    FileNumberReg.Free;
   end;
-  ProgressBar1.Position := 0;
+
+
 
 end;
 
